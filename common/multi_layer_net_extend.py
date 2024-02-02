69b0495df07eb547e3aad16e2e97de6d9474ede5
Towa Kamiyama <132875389+tototo1446@users.noreply.github.com>
2024-02-02 11:47:22 +0900

    Update multi_layer_net_extend.py

--- common/multi_layer_net_extend.py
+++ common/multi_layer_net_extend.py
@@ -1,6 +1,5 @@
-# coding: utf-8
 import sys, os
-sys.path.append(os.pardir) # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir) 
 import numpy as np
 from collections import OrderedDict
 from common.layers import *
