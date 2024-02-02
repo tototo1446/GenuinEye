dc41496365000f67ad4126ce317eeb7094cceac3
Towa Kamiyama <132875389+tototo1446@users.noreply.github.com>
2024-02-02 11:46:34 +0900

    Update multi_layer_net.py

--- common/multi_layer_net.py
+++ common/multi_layer_net.py
@@ -1,6 +1,5 @@
-# coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+sys.path.append(os.pardir) 
 import cupy as cp
 from collections import OrderedDict
 from common.layers import *
